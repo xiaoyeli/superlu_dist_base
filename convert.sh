b958e51cbb84d93855888c8c8c871db178d4bf74
Yang Liu <liuyangzhuan@gmail.com>
2023-12-15 23:14:50 -0800

    minor fix for single precision generation

--- convert.sh
+++ convert.sh
@@ -66,14 +66,9 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 ./extract -b SRC/xscatter3d.c.base -o ../output/SRC/dscatter3d.c precision=DOUBLE
 ./extract -b SRC/xsp_blas2_dist.c.base -o ../output/SRC/dsp_blas2_dist.c precision=DOUBLE
 ./extract -b SRC/xsp_blas3_dist.c.base -o ../output/SRC/dsp_blas3_dist.c precision=DOUBLE
-./extract -b SRC/xsuperlu_blas.c.base -o ../output/SRC/dsuperlu_blas.c.base precision=DOUBLE
+./extract -b SRC/xsuperlu_blas.c.base -o ../output/SRC/dsuperlu_blas.c precision=DOUBLE
 ./extract -b SRC/pxgssvx3d.c.base -o ../output/SRC/pdgssvx3d.c precision=DOUBLE
 ./extract -b SRC/x_c2cpp_GetHWPM.cpp.base -o ../output/SRC/d_c2cpp_GetHWPM.cpp precision=DOUBLE
-
-
-
-
-
 
 
 ./extract -b EXAMPLE/pxdrive.c.base -o ../output/EXAMPLE/pddrive.c precision=DOUBLE
@@ -94,7 +89,6 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 ./extract -b EXAMPLE/xcreate_matrix.c.base -o ../output/EXAMPLE/dcreate_matrix.c precision=DOUBLE
 ./extract -b EXAMPLE/xreadhb.c.base -o ../output/EXAMPLE/dreadhb.c precision=DOUBLE
 ./extract -b EXAMPLE/xreadtriple.c.base -o ../output/EXAMPLE/dreadtriple.c precision=DOUBLE
-
 
 
 ./extract -b TEST/pxtest.c.base -o ../output/TEST/pdtest.c precision=DOUBLE
@@ -122,8 +116,6 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 ./extract -b SRC/xmemory_dist.c.base -o ../output/SRC/zmemory_dist.c precision=DCOMPLEX
 ./extract -b SRC/pxgssvx_ABglobal.c.base -o ../output/SRC/pzgssvx_ABglobal.c precision=DCOMPLEX
 ./extract -b SRC/xsuperlu_gpu.cu.base -o ../output/SRC/zsuperlu_gpu.cu precision=DCOMPLEX
-
-
 
 ./extract -b SRC/pxgstrs_lsum.c.base -o ../output/SRC/pzgstrs_lsum.c precision=DCOMPLEX
 ./extract -b SRC/pxdistribute.c.base -o ../output/SRC/pzdistribute.c precision=DCOMPLEX
@@ -173,7 +165,7 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 ./extract -b SRC/xgsequ_dist.c.base -o ../output/SRC/zgsequ_dist.c precision=DCOMPLEX
 ./extract -b SRC/xlangs_dist.c.base -o ../output/SRC/zlangs_dist.c precision=DCOMPLEX
 ./extract -b SRC/xlaqgs_dist.c.base -o ../output/SRC/zlaqgs_dist.c precision=DCOMPLEX
-./extract -b SRC/xldperm_dist.c.base -o ../output/SRC/zldperm_dist.c.base precision=DCOMPLEX
+./extract -b SRC/xldperm_dist.c.base -o ../output/SRC/zldperm_dist.c precision=DCOMPLEX
 ./extract -b SRC/xlustruct_gpu.h.base -o ../output/SRC/zlustruct_gpu.h precision=DCOMPLEX
 ./extract -b SRC/xnrformat_loc3d.c.base -o ../output/SRC/znrformat_loc3d.c precision=DCOMPLEX
 ./extract -b SRC/xreadhb.c.base -o ../output/SRC/zreadhb.c precision=DCOMPLEX
@@ -184,13 +176,9 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 ./extract -b SRC/xscatter3d.c.base -o ../output/SRC/zscatter3d.c precision=DCOMPLEX
 ./extract -b SRC/xsp_blas2_dist.c.base -o ../output/SRC/zsp_blas2_dist.c precision=DCOMPLEX
 ./extract -b SRC/xsp_blas3_dist.c.base -o ../output/SRC/zsp_blas3_dist.c precision=DCOMPLEX
-./extract -b SRC/xsuperlu_blas.c.base -o ../output/SRC/zsuperlu_blas.c.base precision=DCOMPLEX
+./extract -b SRC/xsuperlu_blas.c.base -o ../output/SRC/zsuperlu_blas.c precision=DCOMPLEX
 ./extract -b SRC/pxgssvx3d.c.base -o ../output/SRC/pzgssvx3d.c precision=DCOMPLEX
 ./extract -b SRC/x_c2cpp_GetHWPM.cpp.base -o ../output/SRC/z_c2cpp_GetHWPM.cpp precision=DCOMPLEX
-
-
-
-
 
 ./extract -b EXAMPLE/pxdrive3d.c.base -o ../output/EXAMPLE/pzdrive3d.c precision=DCOMPLEX
 ./extract -b EXAMPLE/pxdrive3d1.c.base -o ../output/EXAMPLE/pzdrive3d1.c precision=DCOMPLEX
@@ -213,4 +201,88 @@ sed -i 's/[[:blank:]]*$//' TEST/*.*
 
 ./extract -b TEST/pxtest.c.base -o ../output/TEST/pztest.c precision=DCOMPLEX
 
+
+
+
+./extract -b SRC/pxgstrs_lsum.c.base -o ../output/SRC/psgstrs_lsum.c precision=SINGLE
+./extract -b SRC/pxgstrs_lsum_cuda.cu.base -o ../output/SRC/psgstrs_lsum_cuda.cu precision=SINGLE
+./extract -b SRC/pxdistribute.c.base -o ../output/SRC/psdistribute.c precision=SINGLE
+./extract -b SRC/pxdistribute3d.c.base -o ../output/SRC/psdistribute3d.c precision=SINGLE
+./extract -b SRC/x3DPartition.c.base -o ../output/SRC/s3DPartition.c precision=SINGLE
+./extract -b SRC/xdistribute.c.base -o ../output/SRC/sdistribute.c precision=SINGLE
+./extract -b SRC/pxgstrs.c.base -o ../output/SRC/psgstrs.c precision=SINGLE
+./extract -b SRC/pxgstrs3d.c.base -o ../output/SRC/psgstrs3d.c precision=SINGLE
+./extract -b SRC/pxgstrs_Bglobal.c.base -o ../output/SRC/psgstrs_Bglobal.c precision=SINGLE
+./extract -b SRC/pxgstrs1.c.base -o ../output/SRC/psgstrs1.c precision=SINGLE
+./extract -b SRC/pxsymbfact_distdata.c.base -o ../output/SRC/pssymbfact_distdata.c precision=SINGLE
+./extract -b SRC/pxutil.c.base -o ../output/SRC/psutil.c precision=SINGLE
+./extract -b SRC/superlu_xdefs.h.base -o ../output/SRC/superlu_sdefs.h precision=SINGLE
+./extract -b SRC/pxgssvx.c.base -o ../output/SRC/psgssvx.c precision=SINGLE
+./extract -b SRC/xSchCompUdt-2Ddynamic.c.base -o ../output/SRC/sSchCompUdt-2Ddynamic.c precision=SINGLE
+./extract -b SRC/xlook_ahead_update.c.base -o ../output/SRC/slook_ahead_update.c precision=SINGLE
+./extract -b SRC/pxgstrf.c.base -o ../output/SRC/psgstrf.c precision=SINGLE
+./extract -b SRC/pxgstrf2.c.base -o ../output/SRC/psgstrf2.c precision=SINGLE
+./extract -b SRC/pxgstrf3d.c.base -o ../output/SRC/psgstrf3d.c precision=SINGLE
+./extract -b SRC/xutil_dist.c.base -o ../output/SRC/sutil_dist.c precision=SINGLE
+./extract -b SRC/xmemory_dist.c.base -o ../output/SRC/smemory_dist.c precision=SINGLE
+./extract -b SRC/pxgssvx_ABglobal.c.base -o ../output/SRC/psgssvx_ABglobal.c precision=SINGLE
+./extract -b SRC/xtreeFactorization.c.base -o ../output/SRC/streeFactorization.c precision=SINGLE
+./extract -b SRC/xtreeFactorizationGPU.c.base -o ../output/SRC/streeFactorizationGPU.c precision=SINGLE
+./extract -b SRC/xtrfAux.c.base -o ../output/SRC/strfAux.c precision=SINGLE
+./extract -b SRC/xtrfCommWrapper.c.base -o ../output/SRC/strfCommWrapper.c precision=SINGLE
+./extract -b SRC/pxGetDiagU.c.base -o ../output/SRC/psGetDiagU.c precision=SINGLE
+./extract -b SRC/pxgsmv.c.base -o ../output/SRC/psgsmv.c precision=SINGLE
+./extract -b SRC/pxgsmv_AXglobal.c.base -o ../output/SRC/psgsmv_AXglobal.c precision=SINGLE
+./extract -b SRC/pxgsrfs.c.base -o ../output/SRC/psgsrfs.c precision=SINGLE
+./extract -b SRC/pxgsrfs_ABXglobal.c.base -o ../output/SRC/psgsrfs_ABXglobal.c precision=SINGLE
+./extract -b SRC/xcommunication_aux.c.base -o ../output/SRC/scommunication_aux.c precision=SINGLE
+./extract -b SRC/xstatic_schedule.c.base -o ../output/SRC/sstatic_schedule.c precision=SINGLE
+./extract -b SRC/pxlangs.c.base -o ../output/SRC/pslangs.c precision=SINGLE
+./extract -b SRC/pxlaqgs.c.base -o ../output/SRC/pslaqgs.c precision=SINGLE
+./extract -b SRC/xssvx3dAux.c.base -o ../output/SRC/sssvx3dAux.c precision=SINGLE
 ./extract -b SRC/xsuperlu_gpu.cu.base -o ../output/SRC/ssuperlu_gpu.cu precision=SINGLE
+./extract -b SRC/pxgsequ.c.base -o ../output/SRC/psgsequ.c precision=SINGLE
+./extract -b SRC/xmyblas2.c.base -o ../output/SRC/xmyblas2.c precision=SINGLE
+./extract -b SRC/xmyblas2_dist.c.base -o ../output/SRC/xmyblas2_dist.c precision=SINGLE
+./extract -b SRC/xmyblas2_dist.c.base -o ../output/SRC/xmyblas2_dist.c precision=SINGLE
+./extract -b SRC/xreadMM.c.base -o ../output/SRC/sreadMM.c precision=SINGLE
+./extract -b SRC/px3dcomm.c.base -o ../output/SRC/ps3dcomm.c precision=SINGLE
+./extract -b SRC/pxdistribute-aux3d.c.base -o ../output/SRC/psdistribute-aux3d.c precision=SINGLE
+./extract -b SRC/xSchCompUdt-gpu.c.base -o ../output/SRC/sSchCompUdt-gpu.c precision=SINGLE
+./extract -b SRC/xgather.c.base -o ../output/SRC/sgather.c precision=SINGLE
+./extract -b SRC/xgsequ_dist.c.base -o ../output/SRC/sgsequ_dist.c precision=SINGLE
+./extract -b SRC/xlangs_dist.c.base -o ../output/SRC/slangs_dist.c precision=SINGLE
+./extract -b SRC/xlaqgs_dist.c.base -o ../output/SRC/slaqgs_dist.c precision=SINGLE
+./extract -b SRC/xldperm_dist.c.base -o ../output/SRC/sldperm_dist.c precision=SINGLE
+./extract -b SRC/xlustruct_gpu.h.base -o ../output/SRC/slustruct_gpu.h precision=SINGLE
+./extract -b SRC/xnrformat_loc3d.c.base -o ../output/SRC/snrformat_loc3d.c precision=SINGLE
+./extract -b SRC/xreadhb.c.base -o ../output/SRC/sreadhb.c precision=SINGLE
+./extract -b SRC/xreadrb.c.base -o ../output/SRC/sreadrb.c precision=SINGLE
+./extract -b SRC/xreadtriple.c.base -o ../output/SRC/sreadtriple.c precision=SINGLE
+./extract -b SRC/xreadtriple_noheader.c.base -o ../output/SRC/sreadtriple_noheader.c precision=SINGLE
+./extract -b SRC/xscatter.c.base -o ../output/SRC/sscatter.c precision=SINGLE
+./extract -b SRC/xscatter3d.c.base -o ../output/SRC/sscatter3d.c precision=SINGLE
+./extract -b SRC/xsp_blas2_dist.c.base -o ../output/SRC/ssp_blas2_dist.c precision=SINGLE
+./extract -b SRC/xsp_blas3_dist.c.base -o ../output/SRC/ssp_blas3_dist.c precision=SINGLE
+./extract -b SRC/xsuperlu_blas.c.base -o ../output/SRC/ssuperlu_blas.c precision=SINGLE
+./extract -b SRC/pxgssvx3d.c.base -o ../output/SRC/psgssvx3d.c precision=SINGLE
+./extract -b SRC/x_c2cpp_GetHWPM.cpp.base -o ../output/SRC/s_c2cpp_GetHWPM.cpp precision=SINGLE
+
+./extract -b EXAMPLE/pxdrive.c.base -o ../output/EXAMPLE/psdrive.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3d.c.base -o ../output/EXAMPLE/psdrive3d.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3d1.c.base -o ../output/EXAMPLE/psdrive3d1.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3d2.c.base -o ../output/EXAMPLE/psdrive3d2.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3d3.c.base -o ../output/EXAMPLE/psdrive3d3.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive_ABglobal.c.base -o ../output/EXAMPLE/psdrive_ABglobal.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive1.c.base -o ../output/EXAMPLE/psdrive1.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive1_ABglobal.c.base -o ../output/EXAMPLE/psdrive1_ABglobal.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive2.c.base -o ../output/EXAMPLE/psdrive2.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive2_ABglobal.c.base -o ../output/EXAMPLE/psdrive2_ABglobal.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3.c.base -o ../output/EXAMPLE/psdrive3.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive3_ABglobal.c.base -o ../output/EXAMPLE/psdrive3_ABglobal.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive4.c.base -o ../output/EXAMPLE/psdrive4.c precision=SINGLE
+./extract -b EXAMPLE/pxdrive4_ABglobal.c.base -o ../output/EXAMPLE/psdrive4_ABglobal.c precision=SINGLE
+./extract -b EXAMPLE/xcreate_matrix_perturbed.c.base -o ../output/EXAMPLE/screate_matrix_perturbed.c precision=SINGLE
+./extract -b EXAMPLE/xcreate_matrix.c.base -o ../output/EXAMPLE/screate_matrix.c precision=SINGLE
+./extract -b EXAMPLE/xreadhb.c.base -o ../output/EXAMPLE/sreadhb.c precision=SINGLE
+./extract -b EXAMPLE/xreadtriple.c.base -o ../output/EXAMPLE/sreadtriple.c precision=SINGLE
